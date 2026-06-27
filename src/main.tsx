8f2b510974ad437872db3082322c1544a0e0d028
sammylogs619-a11y <sammylogs619@gmail.com>
2026-06-27 14:43:40 +0000

    Fix blank page: wrap App in AuthProvider, add missing pg dependency

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './lib/auth';
+import { ErrorBoundary } from './lib/error-boundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
