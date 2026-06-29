2b4d266a78da6915c59ecac382f99de19f41f3b2
sammylogs619-a11y <sammylogs619@gmail.com>
2026-06-29 22:44:08 +0100

    Add files via upload

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -184,7 +184,7 @@ export function SiteHeader() {
                   </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                  <Link to="/dashboard?tab=profile" className="cursor-pointer">
+                  <Link to="/profile" className="cursor-pointer">
                     <Settings className="w-4 h-4 mr-2" /> Profile settings
                   </Link>
                 </DropdownMenuItem>
